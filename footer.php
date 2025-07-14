d164eb818fb8c1d3256abfcb4af882594e55f870
Bioniko <85115922+Bioniko@users.noreply.github.com>
2025-07-14 16:08:29 -0600

    restructuracion

---
+++ footer.php
@@ -0,0 +1,43 @@
+<!-- copyright section start -->
+<div class="copyright_section">
+    <div class="container">
+    <p class="copyright_text"></p>
+    </div>
+</div>
+<!-- copyright section end -->
+<!-- Javascript files-->
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/jquery-3.0.0.min.js"></script>
+<script src="js/plugin.js"></script>
+<!-- sidebar -->
+<script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+<script src="js/custom.js"></script>
+<!-- javascript --> 
+<script src="js/owl.carousel.js"></script>
+<script>
+    $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:35,
+    nav:true,
+    center: true,
+    responsive:{
+        0:{
+            items:1,
+            margin:0
+            },
+        575:{
+            items:1,
+            margin:0
+        },
+        768:{
+            items:3,
+            margin:0
+        },
+        1000:{
+            items:3
+        }
+    }
+    })   
+</script> 
--- menu.php
+++ menu.php
@@ -1,7 +1,14 @@
-<div class=et_pb_menu__menu>
- <nav class=et-menu-nav><ul id=menu-main-menu class="et-menu nav"><li class="et_pb_menu_page_id-home menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-279453"><a href="index.php" aria-current=page>Inicio</a></li>
-<li class="et_pb_menu_page_id-279311 menu-item menu-item-type-post_type menu-item-object-page menu-item-279354"><a href="soluciones-y-servicios.php">Soluciones y Servicios</a></li>
-<li class="et_pb_menu_page_id-279280 menu-item menu-item-type-post_type menu-item-object-page menu-item-279355"><a href="unete-a-nuestro-equipo.php">Únete a Nuestro Equipo</a></li>
-<li class="et_pb_menu_page_id-279255 menu-item menu-item-type-post_type menu-item-object-page menu-item-279356"><a href="cotiza-con-nosotros.php">Cotiza con Nosotros</a></li>
-</ul></nav>
- </div>
+<ul class="navbar-nav mr-auto">
+    <li class="nav-item active">
+        <a class="nav-link" href="index.php">Inicio</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="index.php">Soluciones y Servicios</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="index.php">Únete a Nuestro Equipo</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="index.php">Cotiza con Nosotros</a>
+    </li>
+</ul>
