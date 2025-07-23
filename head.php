d929ef182c1dc6f2497cab3e369ca3a21c1f3cc4
Bioniko <85115922+Bioniko@users.noreply.github.com>
2025-07-23 10:02:08 -0600

    verificacion

--- head.php
+++ head.php
@@ -1,4 +1,5 @@
 <!-- basic -->
+      <meta name="google-site-verification" content="qt7rB-HFahPsIurqBfBOgC0ZyIrWubQWMs4-SVMdTkA" />
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1">
