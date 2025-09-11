b7974e77b3efeafc309d311f9ff2d3e6931b8fbf
Bioniko <85115922+Bioniko@users.noreply.github.com>
2025-09-11 08:03:04 -0600

    Update enviar_formulario.php

--- enviar_formulario.php
+++ enviar_formulario.php
@@ -42,7 +42,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $mail->addAddress('info@bpohn.com'); // Destinatario
 
         $mail->isHTML(true);
-        $mail->Subject = "Nuevo mensaje de Alerta Security Group";
+        $mail->Subject = "Nuevo mensaje de BPOHN";
         $mail->Body    = "
             <h3>Nuevo mensaje recibido</h3>
             <p><strong>Nombre:</strong> {$nombre}</p>
