b28465688b125a744dc1d1bab85eed7d5728d53e
Bioniko <85115922+Bioniko@users.noreply.github.com>
2025-10-07 11:52:15 -0600

    update index

--- cotiza-con-nosotros.php
+++ cotiza-con-nosotros.php
@@ -19,7 +19,7 @@
       <div class="header_section">
          <div class="container-fluid">
             <nav class="navbar navbar-expand-lg navbar-light bg-light" style="justify-content: space-evenly;">
-               <a class="navbar-brand" href="index.html"><img src="images/img/Logo-BPO-Color.png"></a>
+               <a class="navbar-brand" href="index.php"><img src="images/img/Logo-BPO-Color.png"></a>
                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                <span class="navbar-toggler-icon"></span>
                </button>
